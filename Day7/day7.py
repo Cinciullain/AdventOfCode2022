4c4380836e5f96661ec5c3143b16dad2c11fea3e
Cinciullain <casula.alessandro@outlook.com>
2022-12-13 13:25:51 +0100

    day7 da completare

--- Day7/day7.py
+++ Day7/day7.py
@@ -11,25 +11,30 @@ for i in range(len(righeFile)):
 # Devo trovare il numero di cartelle la cui somma totale della dimensione sia >= 100000
 # Devo trovare un modo per salvare tutti i dati e potervi accedere in modo efficente, come una hashmap o un dizionario
 
-dirCorrente = ""
+dirCorrente = []
 dirNuova = ""
+listaBase = []
 for riga in righeFile:
     riga = riga.split(" ")
     if riga[0] == "$":
         if riga[1] == "cd":
-            print("siamo in cd")
+            # siamo in cd
 
             if riga[2] == "..":
                 print("cartella prima")
-
+                dirCorrente.pop()
             else:
                 print("cartella indicata")
+                dirCorrente.append(riga[2])
 
         else:
-            print("siamo in ls")
+            continue
+            # print("siamo in ls")
 
     elif riga[0] == "dir":
         print("crea cartella")
+        listaSupporto = [riga[1]]
+        listaBase.append(listaSupporto)
 
     else:
         print("crea file")
